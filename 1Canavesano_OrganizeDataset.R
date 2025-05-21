82e0cc108aef52977fba3959b8dc62b15466e675
Alec Gallo <131463445+alecgallo@users.noreply.github.com>
2025-05-21 10:28:49 +0200

    Update 1Canavesano_OrganizeDataset.R

--- 1Canavesano_OrganizeDataset.R
+++ 1Canavesano_OrganizeDataset.R
@@ -11,25 +11,25 @@ library(dplyr)
 #--------------------------------------
 # load spreadsheet with formant values for each participant
 
-data <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker1formants.csv")
-data1 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker2formants.csv")
-data2 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker3formants.csv")
-data3 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker4formants.csv")
-data4 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker5formants.csv")
-data5 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker6formants.csv")
-data6 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker7formants.csv")
-data7 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker8formants.csv")
-data8 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker9formants.csv")
-data9 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker10formants.csv")
-data10 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker11formants.csv")
-data11 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker12formants.csv")
-data12 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker13formants.csv")
-data13 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker14formants.csv")
-data14 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker15formants.csv")
-data15 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker16formants.csv")
-data16 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker17formants.csv")
-data17 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker18formants.csv")
-data18 <- read.csv("E:/ProgettoRivarolese/FormantExtraction/speaker19formants.csv")
+data <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X01formants.xlsx")
+data1 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X02formants.xlsx")
+data2 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X03formants.xlsx")
+data3 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X04formants.xlsx")
+data4 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X05formants.xlsx")
+data5 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X06formants.xlsx")
+data6 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X07formants.xlsx")
+data7 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X08formants.xlsx")
+data8 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X09formants.xlsx")
+data9 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X10formants.xlsx")
+data10 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X11formants.xlsx")
+data11 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X12formants.xlsx")
+data12 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X13formants.xlsx")
+data13 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X14formants.xlsx")
+data14 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X15formants.xlsx")
+data15 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X16formants.xlsx")
+data16 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X17formants.xlsx")
+data17 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X18formants.xlsx")
+data18 <- read.xlsx("D:/ProgettoRivarolese/FormantExtraction_Duration/X19formants.xlsx")
 
 #combine datasets
 data <- rbind(data, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18)
@@ -66,9 +66,14 @@ data1 <- subset(data, phoneme %in% c("a", "e", "ə", "ɛ", "ø", "i", "o", "u", 
 
 # filter the final "i" of "ninsøi"
 data1 <- data1[!(data1$word == "ninsøi" & data1$phoneme == "i"), ]
+data1 <- data1[!(data1$word == "øi" & data1$phoneme == "i"), ]
+data_cleaned <- data %>%
+  filter(!(type == "unstressed" & phoneme == "ɛ"))
+
 
 # rename column file with speaker
 names(data1)[names(data1) == "file"] <- "speaker"
 
-saveRDS(data1, file = "E:/ProgettoRivarolese/data1.rds")
+
+saveRDS(data1, file = "D:/ProgettoRivarolese/data1.rds")
 
